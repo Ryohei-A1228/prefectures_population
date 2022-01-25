140b25b8e9634c20fcefcfa52a3a8461488e0be2
Ryohei-A1228 <87964992+Ryohei-A1228@users.noreply.github.com>
2022-01-26 03:48:08 +0900

    最初のコミット、都道府県名の取得・表示・チェックボックス

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect }  from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [prefectures, setPrefectures] = React.useState([]);
+  const [populations, setPopulations] = React.useState([]);
+  const [selectedPref, setSelectedPref] = React.useState({});
+
+  //データ取得
+  axios.get("https://opendata.resas-portal.go.jp/api/v1/prefectures"
+  ,{
+    headers: {
+      'X-API-KEY': "Uqstu8dEiYopWz7qozQExMHaGmih6v6zx5KsDz15"
+    }
+  }
+  ).then(res => {
+    setPrefectures(res.data.result);
+  });
+
+
+  const CheckBox = ({id, value, checked, onChange}) => {
+    return (
+      <input
+        id={id}
+        type="checkbox"
+        name="inputNames"
+        value={value}
+        onChange={onChange}
+        checked={checked}
+      />
+    )
+  }
+
+  const handleChange = e => {
+    setSelectedPref({
+      ...selectedPref,
+      [e.target.id]: e.target.checked
+    })
+    console.log('selectedPrefs:', selectedPref)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <form>
+        {prefectures.map((item, index) => {
+          index += 1
+          return (
+            <label htmlFor={`id_${index}`} key={`key_${index}`} className='prefCheck'>
+              <CheckBox
+                id={index}
+                value={item}
+                onChange={handleChange}
+                checked={selectedPref[item.prefCode]}
+              />
+              {item.prefName}
+            </label>
+          )
+        })}
+      </form>
     </div>
   );
 }
