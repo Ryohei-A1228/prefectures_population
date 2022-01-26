6db29a05740177a496aee771458b5e188b5cecbb
Ryohei-A1228 <87964992+Ryohei-A1228@users.noreply.github.com>
2022-01-26 20:09:46 +0900

    highchartsの設定

--- src/App.js
+++ src/App.js
@@ -1,64 +1,115 @@
-import React, { useEffect }  from 'react';
-import axios from 'axios';
+import React, {useEffect} from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 function App() {
 
   const [prefectures, setPrefectures] = React.useState([]);
-  const [populations, setPopulations] = React.useState([]);
-  const [selectedPref, setSelectedPref] = React.useState({});
+  const [selectedPref, setSelectedPref] = React.useState(Array(47).fill(false));
+  const [prefSeries, setPrefSeries] = React.useState([]);
 
   //データ取得
-  axios.get("https://opendata.resas-portal.go.jp/api/v1/prefectures"
+  useEffect(()=>{fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures"
   ,{
     headers: {
-      'X-API-KEY': "Uqstu8dEiYopWz7qozQExMHaGmih6v6zx5KsDz15"
+      'X-API-KEY': "PZ1Mxii5zb4Z3aUtq250ncgRAfnTxCoTeW5UXpZz"
     }
   }
-  ).then(res => {
-    setPrefectures(res.data.result);
-  });
+  )
+  .then(response => response.json())
+  .then(res => {
+    setPrefectures(res.result);
+  })}, []);
 
-
-  const CheckBox = ({id, value, checked, onChange}) => {
-    return (
-      <input
-        id={id}
-        type="checkbox"
-        name="inputNames"
-        value={value}
-        onChange={onChange}
-        checked={checked}
-      />
-    )
-  }
-
-  const handleChange = e => {
+  /** const handleChange = e => {
     setSelectedPref({
       ...selectedPref,
       [e.target.id]: e.target.checked
     })
     console.log('selectedPrefs:', selectedPref)
-  }
+  } **/
 
+  const selection = (index) => {
+    const selectedPref_copy = selectedPref.slice();
+    selectedPref_copy[index] = !selectedPref_copy[index];
+
+    if (!selectedPref[index]) {
+      fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${index+1}`,
+        {
+          headers: { 'X-API-KEY': "PZ1Mxii5zb4Z3aUtq250ncgRAfnTxCoTeW5UXpZz" }
+        }
+      )
+        .then(response => response.json())
+        .then(res => {
+          let tmp = [];
+          Object.keys(res.result.data[1].data).forEach(i => {
+            tmp.push(res.result.data[1].data[i].value);
+          });
+          const res_series = {
+            name: prefectures[index].prefName,
+            data: tmp
+          };
+          setSelectedPref(selectedPref_copy);
+          setPrefSeries(res_series);
+        });
+    } else {
+      const prefSeries_copy = prefSeries.slice();
+      for (let i = 0; i < prefSeries_copy.length; i++) {
+        if (prefSeries_copy[i].name === prefectures[index].prefName) {
+          prefSeries_copy.splice(i, 1);
+        }
+      }
+      setSelectedPref(selectedPref_copy);
+      setPrefSeries(prefSeries_copy);
+    }
+  } 
+
+  const CheckBox = (props) => {
+    return (
+      <input
+        type="checkbox"
+        name="inputNames"
+        checked={props.checked}
+        onChange={props.onChange}
+      />
+    )
+  }
+  
+  const obj = prefectures;
+  const options = {
+    title: {
+      text: '総人口増減'
+    },
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false
+        },
+        pointInterval: 5,
+        pointStart: 1980
+      }
+    },
+    series: prefSeries
+  };
 
   return (
     <div className='main'>
       <form>
-        {prefectures.map((item, index) => {
-          index += 1
+        {obj.map((item, index) => {
           return (
-            <label htmlFor={`id_${index}`} key={`key_${index}`} className='prefCheck'>
+            <label htmlFor={`id_${index}`} key={`id_${index}`} className='prefCheck'>
               <CheckBox
                 id={index}
                 value={item}
-                onChange={handleChange}
-                checked={selectedPref[item.prefCode]}
+                onChange={() => selection(item.prefCode - 1)}
+                checked={selectedPref[item.prefCode - 1]}
               />
               {item.prefName}
             </label>
           )
         })}
       </form>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
 }
